6001f9a7bc6087d375173a7920ee52b713ed9bc6
bhartalduoc <ba.hartal@duocuc.cl>
2023-06-17 00:42:29 -0400

    html noticias

--- pjcaosnews/caosnews/urls.py
+++ pjcaosnews/caosnews/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('index',views.index,name='index'),
     path('categorias',views.index,name='categorias'),
     path('periodistas',views.index,name='periodistas'),
-    path('contacto',views.index,name='contacto')
+    path('contacto',views.index,name='contacto'),
+    path('noticia1',views.index,name='noticia1')
 
 ]
--- pjcaosnews/caosnews/views.py
+++ pjcaosnews/caosnews/views.py
@@ -17,3 +17,39 @@ def periodistas (request):
 def contacto (request):
     context = {}
     return render (request,'caosnews/contacto.html',context)
+
+def noticia1 (request):
+    context = {}
+    return render (request,'caosnews/noticia1.html',context)
+
+def noticia2 (request):
+    context = {}
+    return render (request,'caosnews/noticia2.html',context)
+
+def noticia3 (request):
+    context = {}
+    return render (request,'caosnews/noticia3.html',context)
+
+def noticia4 (request):
+    context = {}
+    return render (request,'caosnews/noticia4.html',context)
+
+def noticia5 (request):
+    context = {}
+    return render (request,'caosnews/noticia5.html',context)
+
+def noticia6 (request):
+    context = {}
+    return render (request,'caosnews/noticia6.html',context)
+
+def noticia7 (request):
+    context = {}
+    return render (request,'caosnews/noticia7.html',context)
+
+def noticia8 (request):
+    context = {}
+    return render (request,'caosnews/noticia8.html',context)
+
+def noticia9 (request):
+    context = {}
+    return render (request,'caosnews/noticia9.html',context)
